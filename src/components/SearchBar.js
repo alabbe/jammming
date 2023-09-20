4ccb56ad0747b5999691073edecb2b179b0ca929
Agnes <925492+alabbe@users.noreply.github.com>
2023-09-20 10:24:53 +0200

    Add search bar form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function SearchBar() {
-
+    return (
+        <>
+            <form>
+                <label htmlFor="search">Search song</label>
+                <input type="text" id="search"></input>
+                <button type="submit">Search</button>
+            </form>
+        </>
+    );
 }
 
 export default SearchBar;
